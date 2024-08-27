dfdb97524c1b579a40e86a72ed9fdc361cdaf7ba
keroroxx520@gmail.com <keroroxx520@gmail.com>
2024-08-27 15:00:19 +0800

    feat: support cmd line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+use std::env;
 use lc3_zkvm::memory::Memory;
 use lc3_zkvm::register::{RegisterFile, Register};
 use lc3_zkvm::utils::{load_obj_file, execute_program};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 2 {
+        return Err("Usage: program <path_to_obj_file>".into());
+    }
+
+    let obj_file_path = &args[1];
+
     let mut memory = Memory::new();
     let mut registers = RegisterFile::new();
 
     // Load the LC3 object file
-    let origin = load_obj_file("path/to/your/program.obj", &mut memory)?;
+    let origin = load_obj_file(obj_file_path, &mut memory)?;
 
     // Set the PC to the program's origin
     registers.write(Register::PC, origin);
 
-    // Execute the program with debug mode enabled
+    // Execute the program
     match execute_program(&mut memory, &mut registers) {
         Ok(_) => println!("Program executed successfully"),
         Err(e) => println!("Error executing program: {}", e),
