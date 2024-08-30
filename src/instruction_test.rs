cbe87cfffec7e355dd4787af12e2a674029d08d5
keroroxx520@gmail.com <keroroxx520@gmail.com>
2024-08-30 11:58:04 +0800

    chore: cargo fmt

--- src/instruction_test.rs
+++ src/instruction_test.rs
@@ -1,7 +1,8 @@
-use crate::instruction::execute;
-use crate::memory::Memory;
 use crate::opcode::{extract_opcode, Opcode};
-use crate::register::{Register, RegisterFile};
+use crate::register::{RegisterFile, Register};
+use crate::memory::Memory;
+use crate::instruction::execute;
+
 
 #[test]
 fn test_add_instruction() {
@@ -14,8 +15,6 @@ fn test_add_instruction() {
 
     // Encode the ADD instruction (ADD R2, R0, R1)
     let instruction = 0b0001_010_000_000_001;
-    // 0x1401;
-    //0b0001_010_000_001_000;
     assert_eq!(extract_opcode(instruction), Some(Opcode::OP_ADD));
 
     // Execute the instruction
